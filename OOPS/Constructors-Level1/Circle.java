e4f3e3891f254efa05f45e73fd6b4261b8eb868c
Ishendra <ishurai.020805@gmail.com>
2025-09-13 00:29:13 +0530

    .

--- OOPS/Constructors-Level1/Circle.java
+++
@@ -1,6 +0,0 @@
-public class Circle {
-    private double radius;
-    public Circle(){ this(1.0); }
-    public Circle(double radius){ this.radius=radius; }
-    public String toString(){ return "Circle radius:"+radius; }
-}
